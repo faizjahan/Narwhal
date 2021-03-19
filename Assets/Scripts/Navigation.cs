2e48409e6ab567a34861f96a896c805340bc7034
faizjahan <faizjahan@googlemail.com>
2021-03-19 13:43:18 +0200

    Initial commit

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour
+{
+    
+    [SerializeField] GameObject[] panels;
+
+    private void selectPanel(GameObject selected)
+    {
+        for (int i = 0; i < panels.Length; i++)
+        {
+            panels[i].SetActive(false);
+        }
+        selected.SetActive(true);
+    }
+
+    private void Start()
+    {
+        selectPanel(panels[4]);
+    }
+
+    public void BarClick(GameObject activePanel)
+    {
+        selectPanel(activePanel);
+    }
+    
+}
